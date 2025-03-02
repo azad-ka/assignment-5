4c6cd300acf6b98e5e5a33d01c6d91dab6b33e62
azad-ka <azad.ka042@gmail.com>
2025-03-02 16:08:41 +0600

    added current date

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
+// main page and blogs page link
+
 document.getElementById('btn-nav').addEventListener('click', function(){
 
     window.location.href = "./blogs.html";
 })
 
+// random color 
+
 document.getElementById('btn-random-color').addEventListener('click', function(){
     const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = randomColor;
 })
+
+// set current date
+
+let currentDate = new Date();
+let dateTime = currentDate.toDateString();
+document.getElementById('current-date').innerHTML = dateTime;
+
+
