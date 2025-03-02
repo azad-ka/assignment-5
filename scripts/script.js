028185c29c58a44df068115ef3ce97b4097fba6e
azad-ka <azad.ka042@gmail.com>
2025-03-02 14:12:10 +0600

    random color button added

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,8 @@ document.getElementById('btn-nav').addEventListener('click', function(){
 
     window.location.href = "./blogs.html";
 })
+
+document.getElementById('btn-random-color').addEventListener('click', function(){
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+})
